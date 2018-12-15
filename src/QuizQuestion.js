9312e7ee726df9022e6eef3a3a81eddb7aaceb6d
BPatel <bhavinkp@gmail.com>
2018-12-15 12:02:36 -0500

    Added error message - module6

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -2,17 +2,21 @@ import React, { Component } from 'react'
 import QuizQuestionButton from './QuizQuestionButton.js'
 
 class QuizQuestion extends Component{
-  //constructor(props){
-    //super(props)
-    //this.state
-  //}
+  constructor(props){
+    super(props)
+    this.state={incorrectAnswer:false}
+  }
 
   handleClick(buttonText){
+
     if(buttonText === this.props.quiz_question.answer){
-      //console.log("in handleClick")
+      this.setState({incorrectAnswer:false})
       this.props.showNextQuestionHandler()
+    }else {
+      this.setState({incorrectAnswer:true})
     }
   }
+
   render(){
     return(
       <main>
@@ -28,6 +32,8 @@ class QuizQuestion extends Component{
 
           </ul>
         </section>
+        {this.state.incorrectAnswer ? <p className="error">Sorry, that's not right </p>:
+          null}
       </main>
     )
   }
